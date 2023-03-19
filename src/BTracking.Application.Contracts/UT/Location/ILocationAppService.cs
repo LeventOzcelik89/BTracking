2a236e2e98f6d12abaee91f86c7e06d7e625610e
LeventOzcelik89 <Levent.ozcelik@outlook.com>
2023-03-19 21:43:21 +0300

    Location Service

---
+++ src/BTracking.Application.Contracts/UT/Location/ILocationAppService.cs
@@ -0,0 +1,22 @@
+﻿using BTracking.UT.Towns;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+
+namespace BTracking.UT.Location
+{
+    public interface ILocationAppService
+    {
+        Task<PagedResultDto<TownDto>> GetListAsync(GetTownInput input);
+
+        Task<TownDto> GetAsync(Guid id);
+
+        Task DeleteAsync(Guid id);
+
+        Task<TownDto> CreateAsync(TownCreateDto input);
+
+        Task<TownDto> UpdateAsync(Guid id, TownUpdateDto input);
+    }
+}
